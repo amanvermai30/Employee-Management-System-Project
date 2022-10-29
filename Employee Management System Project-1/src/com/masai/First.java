78b32877e36c1b57ffbc4f0a253ea81edcc2d543
Aman Verma <amancreative9598@gmail.com>
2022-10-29 13:04:36 +0530

    first page added

--- Employee Management System Project-1/src/com/masai/First.java
+++
@@ -1,8 +0,0 @@
-package com.masai;
-
-public class First {
-
-    public static void main(String[] args) {
-        System.out.println("I am going to make my first java project Yep!");
-    }
-}
---
+++ Employee Management System Project-1/src/employee/management/system/Splash.java
@@ -0,0 +1,64 @@
+package employee.management.system;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Splash extends JFrame {
+
+    Splash(){
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel heading = new JLabel("EMPLOYEE MANAGEMENT SYSTEM");
+        heading.setBounds(230,20,1200,40);
+        add(heading);
+        heading.setFont(new Font("serif", Font.PLAIN,40));
+        heading.setForeground(Color.CYAN);
+
+
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("Photos/Background.jpg"));
+        Image i1 = imageIcon.getImage().getScaledInstance(1140,650,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon2 = new ImageIcon(i1);
+        JLabel img = new JLabel(imageIcon2);
+        img.setBounds(70,70,1000,500);
+        add(img);
+
+        JButton clickButton = new JButton("CLICK HERE TO CONTINUE");
+        clickButton.setBounds(450,200,200,50);
+        clickButton.setBackground(Color.BLUE);
+        clickButton.setForeground(Color.WHITE);
+
+        Border black = BorderFactory.createLineBorder(Color.CYAN);
+        clickButton.setBorder(black);
+        img.add(clickButton);
+
+
+        setSize(1170,650);
+        setLocation(110,35);
+        setVisible(true);
+
+        while (true){
+            heading.setVisible(false);
+            try {
+                  Thread.sleep(500);
+            }catch (Exception ignored){
+
+            }
+            heading.setVisible(true);
+            try {
+                Thread.sleep(500);
+            }catch (Exception ignored){
+
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        new Splash();
+    }
+}
