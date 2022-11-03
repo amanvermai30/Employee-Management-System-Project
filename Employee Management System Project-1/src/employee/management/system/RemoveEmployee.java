c2a847f01894c4e656fdbafcfd08eac5e840cddf
Aman Verma <amancreative9598@gmail.com>
2022-11-03 23:19:13 +0530

    remove employee page is added

---
+++ Employee Management System Project-1/src/employee/management/system/RemoveEmployee.java
@@ -0,0 +1,18 @@
+package employee.management.system;
+
+import javax.swing.*;
+
+public class RemoveEmployee extends JFrame {
+
+    RemoveEmployee(){
+
+
+        setSize(1000,400);
+        setLocation(300,150);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new RemoveEmployee();
+    }
+}
